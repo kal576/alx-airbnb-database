b515a31079b03e634e462e3940f2617785ed49c0
kal576 <cmuuo5864@gmail.com>
2025-05-13 21:50:09 +0300

    Udate perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,34 @@
+-- initial query
+SELECT b.booking_id,
+	b.guest-id,
+	CONCAT(u.first_name, ' ', u.last_name) AS full_name,
+	p.property_id,
+	p.title,
+	pay.payment_id,
+	pay.payment_method,
+	pay.payment_date
+JOIN users u ON u.user_id = b.guest_id
+JOIN properties p ON p.property_id = b.property_id
+LEFT JOIN payments pay ON pay.booking_id = b.booking_id
+ORDER BY b.booking_id;
+
+-- created indexes
+CREATE INDEX idx_bookings_guest_id ON bookings(guest_id)
+CREATE INDEX idx_bookings_properties_id ON bookings(property_id)
+CREATE INDEX idx_payments_booking_id ON payments(booking_id)
+
+--optimized query 
+EXPLAIN SELECT b.booking_id,
+        b.guest-id,
+        CONCAT(u.first_name, ' ', u.last_name) AS full_name,
+        p.property_id,
+        p.title,
+        pay.payment_id,
+        pay.payment_method,
+        pay.payment_date
+JOIN users u ON u.user_id = b.guest_id
+JOIN properties p ON p.property_id = b.property_id
+LEFT JOIN payments pay ON pay.booking_id = b.booking_id
+ORDER BY b.booking_id;
+
+
