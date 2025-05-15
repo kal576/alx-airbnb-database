3cea8ac42ed9161d71d38ff0b174dacb66b806de
kal576 <160145829+kal576@users.noreply.github.com>
2025-05-15 07:18:16 +0300

    Add ORDER BY on subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,21 +1,30 @@
 -- Non-Correlated Subquery
 SELECT 
-    property_id,
-    title
-FROM properties
-WHERE property_id IN (
+    p.property_id,
+    p.title,
+    AVG(r.rating) AS Average_rating
+FROM properties p
+INNER JOIN reviews r ON p.property_id = r.property_id
+WHERE p.property_id IN (
     SELECT property_id
     FROM reviews
     GROUP BY property_id
     HAVING AVG(rating) > 4.0
-);
+)
+GROUP BY property_id, p.title
+ORDER BY property_id;
+
 -- Correlated Subquery
 SELECT 
-    user_id,
-    CONCAT(first_name, ' ', last_name) AS full_name
+    u.user_id,
+    CONCAT(u.first_name, ' ', u.last_name) AS full_name,
+    COUNT(b.booking_id) AS total_bookings
 FROM users u
+INNER JOIN bookings b ON b.guest_id = u.user_id
 WHERE (
     SELECT COUNT(*) 
     FROM bookings b 
     WHERE b.guest_id = u.user_id
-) > 3;
+) > 3
+GROUP BY user_id, full_name
+ORDER BY total_bookings;
