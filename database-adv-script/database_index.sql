c98d4d0a241eb6b55a5247b1b7387753572e9aba
kal576 <160145829+kal576@users.noreply.github.com>
2025-05-15 07:44:09 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -6,7 +6,7 @@ CREATE INDEX idx_bookings_guest_id ON bookings(guest_id);
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
 
 -- properties table indexes
-CREATE INDEX idx_bookings_host_id ON bookings(host_id);
+CREATE INDEX idx_properties_host_id ON properties(host_id);
 CREATE INDEX idx_property_id ON properties(property_id);
 
 -- analysing performance
